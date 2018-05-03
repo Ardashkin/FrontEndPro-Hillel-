a9b11c899bf93727a3c747fcb2a32f00eede263c
Alex_Ardashkin <aleksandr.ardashkin@gmail.com>
2018-05-03 20:38:01 +0300

    333

--- 04 (27.04.2018)/ExtraWork/js/main.js
+++ 04 (27.04.2018)/ExtraWork/js/main.js
@@ -4,7 +4,8 @@ var max, min;
 numberArray.sort(function (a, b)
     {return a - b;}
                 );
-console.log(" Max = " +numberArray[numberArray.length-1] + " PreMax = " +numberArray[numberArray.length-2]);
+console.log(" Max = " +numberArray[numberArray.length-1] + " PreMax = " +numberArray[numberArray.length-2] +
+            " Summ = " +(numberArray[numberArray.length-1] + numberArray[numberArray.length-2]));
 
 max = min = 0;
 for (let i = 0; i < numberArray.length; ++i) {
