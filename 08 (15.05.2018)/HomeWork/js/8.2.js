e1473dce79d8655423cb2ae244929647fa54b378
Alex_Ardashkin <aleksandr.ardashkin@gmail.com>
2018-05-22 22:25:39 +0300

    22.05.2018 22:25

--- 08 (15.05.2018)/HomeWork/js/8.2.js
+++ 08 (15.05.2018)/HomeWork/js/8.2.js
@@ -3,6 +3,23 @@ function Person(namePerson, surnamePerson){
     this.namePerson = namePerson;
     this.surnamePerson = surnamePerson;
 
+    this.add = function(key, value) {
+        return this[key] = value;
+    };
+
+    this.delete = function(key) {
+        if (this[key]) delete this[key];
+    };
+
+    this.has = function(key) {
+        if (this[key]) return true;
+        else return false;
+    };
+
+    this.get = function(key) {
+        if (this[key]) return this[key];
+    };
+
     this.toArray = function() {
         var array = [];
         for (var i in this) {
@@ -12,24 +29,6 @@ function Person(namePerson, surnamePerson){
         }
         return array;
     };
-
-    this.add = function(nameKey, value) {
-        return this[nameKey] = value;
-    }
-
-    this.delete = function(nameKey) {
-        if (this[nameKey]) delete this[nameKey];
-    }
-
-    this.has = function(nameKey) {
-        if (this[nameKey]) return true;
-        else return false;
-    }
-
-    this.get = function(nameKey) {
-        if (this[nameKey]) return this[nameKey];
-    }
-
 }
 
 var Person1 = new Person('Alex', 'Ardashkin');
